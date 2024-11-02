cac5c838ec30b552f5f1d4396f169233f8916dea
Eyo Chen <eyo.chen@amazingtalker.com>
2024-11-02 09:38:33 +0800

    Review: canVisitAllRooms

--- Graph/canVisitAllRooms/go/main.go
+++ Graph/canVisitAllRooms/go/main.go
@@ -21,3 +21,28 @@ func helper(key int, rooms [][]int, isRoomVisited map[int]bool) {
 		helper(curRoom, rooms, isRoomVisited)
 	}
 }
+
+// using BFS
+func CanVisitAllRooms1(rooms [][]int) bool {
+	queue := []int{0}
+	hashTable := map[int]bool{}
+
+	for len(queue) > 0 {
+		n := queue[0]
+		queue = queue[1:]
+
+		if hashTable[n] {
+			continue
+		}
+
+		hashTable[n] = true
+
+		// Directly put all the keys in the current room into the queue
+		// Or
+		// We can manually check each key in the current room
+		// and put the key into the queue if the room is not visited
+		queue = append(queue, rooms[n]...)
+	}
+
+	return len(rooms) == len(hashTable)
+}
