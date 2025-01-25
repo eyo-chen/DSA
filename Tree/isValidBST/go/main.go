ca84ccf6f91deb87c16b321f9c2572c0bf69cfca
Eyo Chen <eyo.chen@amazingtalker.com>
2025-01-25 15:06:19 +0800

    Tree: isValidBST

--- Tree/isValidBST/go/main.go
+++ Tree/isValidBST/go/main.go
@@ -30,3 +30,19 @@ func dfs(node *TreeNode, maxVal, minVal int) bool {
 	// Or we can simply return
 	// return dfs(node.Left, node.Val, minVal) && dfs(node.Right, maxVal, node.Val)
 }
+
+func IsValidBST2(root *TreeNode) bool {
+	return helper(root, math.MinInt, math.MaxInt)
+}
+
+func helper(node *TreeNode, min int, max int) bool {
+	if node == nil {
+		return true
+	}
+
+	if node.Val <= min || node.Val >= max {
+		return false
+	}
+
+	return helper(node.Left, min, node.Val) && helper(node.Right, node.Val, max)
+}
